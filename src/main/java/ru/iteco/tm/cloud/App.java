832a5d58d81c8e6f2fad1a2ace318adb42e2a365
Никита Иохин <iokhin.nik@gmail.com>
2019-07-02 15:57:42 +0000

    add README

---
+++ src/main/java/ru/iteco/tm/cloud/App.java
@@ -0,0 +1,13 @@
+package ru.iteco.tm.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
